4798c8d95d779f335cfc76ad42396c32e0b695cf
FelixBatista <felixb.neto@gmail.com>
2024-02-19 13:41:35 +0100

    corrected licenses

--- webhook_handler.php
+++ webhook_handler.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * @version		$Id: webhooks.php 06/02/2024 FelixBatista $
+ * @package		Joomla
+ * @subpackage	Content
+ * @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
+ * @license		GPLv2, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
 defined('_JEXEC') or die;
 JLog::addLogger(array('text_file' => 'webhooks.log.php'), JLog::ALL, array('webhooks'));
 
